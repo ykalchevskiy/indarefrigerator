17adae046351b93f96f3e6c179029540147c0dbd
Yan Kalchevksiy <yan.kalchevskiy@gmail.com>
2014-04-23 23:15:04 +0700

    Added login_required for admin page.

--- indarefrigerator/products/admin.py
+++ indarefrigerator/products/admin.py
@@ -1,7 +1,16 @@
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.login import current_user
 
 from .models import Product
 from ..extensions import admin, db
 
 
-admin.add_view(ModelView(Product, db.session))
+class ProductAdminView(ModelView):
+    def __init__(self, model, session, **kwargs):
+        super(ProductAdminView, self).__init__(model, session, **kwargs)
+
+    def is_accessible(self):
+        return current_user.is_authenticated()
+
+
+admin.add_view(ProductAdminView(Product, db.session))
